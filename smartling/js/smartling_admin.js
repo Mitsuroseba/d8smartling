48e87829cf9a5014e37cf20d07872536569c9e1a
Artem Shymko <itwebcross@gmail.com>
2013-11-15 19:53:22 +0400

    Parallel workres functonality, check and download tranlation buttons, progress bar JS

--- smartling/js/smartling_admin.js
+++ smartling/js/smartling_admin.js
@@ -5,4 +5,21 @@
 
 (function($) {
 
+  Drupal.behaviors.smartlingProgressbar = {
+    attach: function(context, settings) {
+
+      var progress = '';
+      var is_progressbar = $('.view-smartlig-report .views-field-progress').attr('class');
+      if (typeof is_progressbar !== 'undefined') {
+        $('.view-smartlig-report tbody .views-field-progress').each(function() {
+          progress = $(this).html();
+          $(this).empty();
+          $(this).progressbar({
+            value: parseInt(progress)
+          });
+        });
+      }
+    }
+  }
+
 })(jQuery);
