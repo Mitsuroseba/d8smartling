2c3f9940eddb7131193debce3ccc4b55dc602a53
Artem Shymko <itwebcross@gmail.com>
2013-12-09 13:56:19 +0400

    Updated code: new version of translation supported, solved issues #22, #21 partially, #20, #19, #7 done before

--- smartling/js/smartling_admin.js
+++ smartling/js/smartling_admin.js
@@ -13,7 +13,11 @@
       if (typeof is_progressbar !== 'undefined') {
         $('.view-smartlig-report tbody .views-field-progress').each(function() {
           progress = $(this).html();
+          var progress_string = '<div class="progress-val">' + progress + '</div>';
           $(this).empty();
+          $(this).append(progress_string);
+          $(this).css({'position': 'relative'});
+          $(this).find('.progress-val').css({'display': 'inline-block', 'width': '100%', 'height': '100%', 'text-align': 'center', 'position': 'absolute', 'left': '0'});
           $(this).progressbar({
             value: parseInt(progress)
           });
