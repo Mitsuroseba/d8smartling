0ca7216c968851c30ca15bb335051cbeea4db320
Maxim Bogdanov <sin666m4a1fox@gmail.com>
2014-04-04 14:51:55 +0400

    #79

--- smartling/smartling.demo_content.php
+++ smartling/smartling.demo_content.php
@@ -409,7 +409,7 @@ function get_random_taxonomy_array()
 
 function set_message_watchdog($msg)
 {
-    drupal_set_message($msg);
+    drupal_set_message(t($msg));
     smartling_watchdog($msg, TRUE, array(), WATCHDOG_INFO);
 }
 
