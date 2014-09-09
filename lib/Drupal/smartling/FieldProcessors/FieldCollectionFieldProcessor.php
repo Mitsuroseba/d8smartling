e525b412cca7e66e277d82271352bd099c6a1acf
Konstantin <e-science@mail.ru>
2014-09-09 15:12:42 +0300

    some code to retrieve content from field_collection

--- lib/Drupal/smartling/FieldProcessors/FieldCollectionFieldProcessor.php
+++ lib/Drupal/smartling/FieldProcessors/FieldCollectionFieldProcessor.php
@@ -15,10 +15,27 @@ class FieldCollectionFieldProcessor extends BaseFieldProcessor {
   public function getSmartlingContent() {
     $data = array();
 
+
     if (!empty($this->entity->{$this->fieldName}[$this->language])) {
       foreach ($this->entity->{$this->fieldName}[$this->language] as $delta => $value) {
         $fid = (int)$value['value'];
-        $data[$delta] = field_collection_item_load($fid);
+        $entity = field_collection_item_load($fid);
+
+        $wrapper = entity_metadata_wrapper('field_collection', $entity);
+        $smartling_entity = smartling_entity_load_by_conditions(array(
+          'rid' => $wrapper->getIdentifier(),
+          'target_language' => $this->language,
+        ));
+
+        if (!$smartling_entity) {
+          // @todo verify that entity has language property.
+          $smartling_entity = smartling_create_from_entity($entity, 'field_collection', $entity->langcode, $this->language);
+        }
+        //$smartling_entity = smartling_create_from_entity($entity, $entity->type, $entity->language, $this->language);
+        $processor = smartling_get_entity_processor($smartling_entity);
+        $data[$delta] = $processor->exportContentToTranslation();
+
+
       }
     }
 
