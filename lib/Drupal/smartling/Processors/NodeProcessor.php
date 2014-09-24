5e8aec65284707e3b5221f94308ceda799f4c1b9
Artyom Miroshnik <artyom.miroshnik@propeople.com.ua>
2014-09-24 11:03:47 +0300

    Temp fix for field collections

--- lib/Drupal/smartling/Processors/NodeProcessor.php
+++ lib/Drupal/smartling/Processors/NodeProcessor.php
@@ -35,24 +35,30 @@ class NodeProcessor extends GenericEntityProcessor {
         $node->uid = $this->entity->submitter;
         $node->tnid = $this->contentEntity->nid;
 
+        // @todo Do we need this? clone should do all the stuff.
         $node_fields = field_info_instances('node', $this->contentEntity->type);
         foreach ($node_fields as $field) {
           $node->{$field['field_name']} = $this->contentEntity->{$field['field_name']};
         }
 
-        foreach ($this->getTranslatableFields() as $field_name) {
-          // Still use entity object itself because entity wrapper hardcodes
-          // language and disallow to fetch values from translated fields.
-          // However all entities work with entities in the same way.
-          if (!empty($this->contentEntity->{$field_name}[$this->drupalOriginalLocale])) {
-            $fieldProcessor = $this->fieldProcessorFactory->getProcessor($field_name, $this->contentEntity, $this->drupalEntityType, $this->entity, $this->targetFieldLanguage);
-            $node->{$field_name}[$this->drupalOriginalLocale] = $fieldProcessor->prepareBeforeDownload($this->contentEntity->{$field_name}[$this->drupalOriginalLocale]);
-          }
-        }
-
         $node->translation_source = $this->contentEntity;
 
         node_object_prepare($node);
+        node_save($node);
+
+        foreach ($this->getTranslatableFields() as $field_name) {
+          // Run all translatable fields through prepareBeforeDownload
+          // to make sure that all related logic was triggered.
+          if (!empty($this->contentEntity->{$field_name}[LANGUAGE_NONE])) {
+            $fieldProcessor = $this->fieldProcessorFactory->getProcessor($field_name, $node, $this->drupalEntityType, $this->entity, $this->targetFieldLanguage);
+            // @TODO get rid of harcoded language.
+            $node->{$field_name}[LANGUAGE_NONE] = $fieldProcessor->prepareBeforeDownload($this->contentEntity->{$field_name}[LANGUAGE_NONE]);
+          }
+
+        }
+
+        // Second saving is done for Field Collection field support
+        // that need host entity id.
         node_save($node);
 
         // Update reference to drupal content entity.
