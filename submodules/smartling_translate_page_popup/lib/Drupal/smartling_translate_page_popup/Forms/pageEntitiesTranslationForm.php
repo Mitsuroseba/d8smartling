fd856aa8bb67e75682bbbfeca94b7ce0c1db8e9e
Konstantin <escience88@yahoo.com>
2015-02-17 18:13:44 +0200

    Big part of the modules features.

--- submodules/smartling_translate_page_popup/lib/Drupal/smartling_translate_page_popup/Forms/pageEntitiesTranslationForm.php
+++ submodules/smartling_translate_page_popup/lib/Drupal/smartling_translate_page_popup/Forms/pageEntitiesTranslationForm.php
@@ -13,29 +13,73 @@ class pageEntitiesTranslationForm implements \Drupal\smartling\Forms\FormInterfa
 
   public function buildForm(array $form, array &$form_state) {
     $options = array();
-    //print_r($_SESSION['smartling']['page_entities']);
+
+    $path = drupal_get_path('module', 'smartling_translate_page_popup');
+    $start =<<<EOF
+    <div id="smartling_translate_popup" class="smartling_state_compressed">
+      <div id="smartling_popup_header">
+        <div>
+          <a id="smartling_popup_closebutton" class="helper_button">x</a>
+          <a id="smartling_popup_sizebutton" class="helper_button">_</a>
+          <a href="http://www.smartling.com/" target="blank"><img src="/$path/static/logo.png" height="30px"></a>
+        </div>
+      </div>
+	  <div id="smartling_popup_content" class="not_visible">
+	    <span id="smartling_stats"><a>Stats</a></span>
+	    <div id="translation_result"></div>
+EOF;
+    $end =<<<EOF
+      </div>
+    </div>
+EOF;
+
+
+    if (empty($_SESSION['smartling']['page_entities'] )) {
+      $form['smartling']['start'] = array(
+        '#prefix' => $start,
+        '#markup' => t('It seems that there are no items on the page that Smartling can translate. This can happen if <a href="@link">Smartling module is not configured</a> properly or there are no content items in the <b>site\'s default</b> or <b>neutral</b> language.', array('@link' => url('admin/config/regional/smartling'))),
+        '#suffix' => $end,
+        '#attached' => array('css' => array(drupal_get_path('module', 'smartling_translate_page_popup') . '/static/smartling_translate_page_popup.css'),
+          'js'  => array(drupal_get_path('module', 'smartling_translate_page_popup') . '/static/smartling_translate_page_popup.js',))
+      );
+      return $form;
+    }
+
     foreach ($_SESSION['smartling']['page_entities'] as $id => $title) {
       list($entity_id, $entity_type) = explode('_||_', $id);
-      smartling_entity_load_by_conditions(array(
+      $res = smartling_entity_load_all_by_conditions(array(
         'rid' => $entity_id,
         'entity_type' => $entity_type,
-        //'target_language' => $entity_data->target_language,
       ));
 
-      $options [$id] = $title;
+      $data = '';
+      if (!empty($res)) {
+        foreach($res as $dt) {
+          $data .= $dt->target_language . ' - ' . $dt->progress . '%; ';
+        }
+      }
+
+      $options [$id] = $title . " <span class='entity_type'>($entity_type)</span> <span class='entity_progress'>($data)</span>";
     }
     unset($_SESSION['smartling']['page_entities']);
+
+    $form['smartling']['start'] = array(
+      '#markup' =>$start
+    );
 
     $form['smartling']['items'] = array(
       '#type' => 'checkboxes',
       '#options' => $options,//$_SESSION['smartling']['page_entities'], //drupal_map_assoc(array(t('SAT'), t('ACT'))),
-      '#title' => t('What items would you like to translate?'),
+      '#title' => t('Content items'),
+      //'#description' => t('What items would you like to translate?'),
+      '#required' => TRUE,
     );
 
     $form['smartling']['languages'] = array(
       '#type' => 'checkboxes',
       '#options' => smartling_language_options_list(),//$_SESSION['smartling']['page_entities'], //drupal_map_assoc(array(t('SAT'), t('ACT'))),
       '#title' => t('Languages'),
+      '#required' => TRUE,
     );
 
     $form['smartling']['submit'] = array(
@@ -46,21 +90,36 @@ class pageEntitiesTranslationForm implements \Drupal\smartling\Forms\FormInterfa
       '#value' => t('Translate'),
     );
 
+    $form['smartling']['end'] = array(
+      '#markup' =>$end,
+      '#attached' => array('css' => array(drupal_get_path('module', 'smartling_translate_page_popup') . '/static/smartling_translate_page_popup.css'),
+                           'js'  => array(drupal_get_path('module', 'smartling_translate_page_popup') . '/static/smartling_translate_page_popup.js',))
+
+    );
+
     return $form;
   }
 
   public function validateForm(array &$form, array &$form_state) {
-
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, array &$form_state) {
+    $errors = form_get_errors();
+    if ($errors) {
+      $commands[] = ajax_command_replace('#translation_result', '<div id="translation_result" class="failed">' . implode(" ", $errors) . '</div>');
+      return array('#type' => 'ajax', '#commands' => $commands);
+    }
     //print_r($form_state['input']);
     //die('hi');
     //$elem
-    $commands[] = ajax_command_replace('#smartling-translate-page-popup-form', print_r($form_state['input'], TRUE));
+    //print_r($form_state['input'], TRUE)
+    //drupal_set_message('hi', 'error');
+    //rgb(194, 82, 20)
+    $commands[] = (mt_rand(0,1)==0)?ajax_command_replace('#translation_result', '<div id="translation_result" class="success">Everything is ok!</div>'):
+      ajax_command_replace('#translation_result', '<div id="translation_result" class="failed">Something is wrong!</div>');
   return array(
     '#type' => 'ajax',
     '#commands' => $commands,
